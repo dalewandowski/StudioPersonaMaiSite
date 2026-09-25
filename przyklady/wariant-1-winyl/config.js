7beacdad86890ee621f2ccf31eceacfa6fa1e515
Damian Lewandowski <d.lewandowski94@onet.pl>
2026-09-25 20:48:42 +0200

    Fix links from unsplasch

--- przyklady/wariant-1-winyl/config.js
+++ przyklady/wariant-1-winyl/config.js
@@ -9,23 +9,23 @@ const CONFIG = {
   signature: "Marek",
   photos: [
     {
-      src: "https://images.unsplash.com/photo-1583939003579-730e3918a45a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdlZGRpbmd8ZW58MHx8MHx8fDA%3D",
+      src: "https://images.unsplash.com/photo-1583939003579-730e3918a45a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0",
       caption: "Ślub, 2021",
     },
     {
-      src: "https://images.unsplash.com/photo-1617376431454-8195cf1fd668?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://images.unsplash.com/photo-1617376431454-8195cf1fd668?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0",
       caption: "Wakacje nad morzem",
     },
     {
-      src: "https://plus.unsplash.com/premium_photo-1680300960814-f684429a9cad?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://plus.unsplash.com/premium_photo-1680300960814-f684429a9cad?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0",
       caption: "Pierwsze mieszkanie",
     },
     {
-      src: "https://images.unsplash.com/photo-1575240539571-c2fa80c9ca3e?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://images.unsplash.com/photo-1575240539571-c2fa80c9ca3e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0",
       caption: "Wigilia u rodziców",
     },
     {
-      src: "https://plus.unsplash.com/premium_photo-1664053011453-bac12a655c04?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://plus.unsplash.com/premium_photo-1664053011453-bac12a655c04?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0",
       caption: "Dziś",
     },
   ],
